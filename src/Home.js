27e153635007d6b56c420f8d9b73528159035f88
George <gwong323@yahoo.com>
2019-09-14 15:56:47 +0100

    practicing redux

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,20 @@
 import React, { Component } from "react";
 import Footer from "./Footer";
 import { useSelector } from "react-redux";
+import increment from "./actions";
 
 class Home extends Component {
   state = {};
   // const counterReducer = useSelector(state => state.counterReducer);
   whatever = () => {};
 
+  test = () => {};
+
   render() {
     return (
       <div className="home">
-        <h1>Home {useSelector(state => state.counterReducer)} </h1>
-
+        <h1>Home {this.test()}</h1>
+        <button>+</button>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
           deleniti praesentium fugiat, neque ea ullam modi quaerat quam officia
